2dcc795b927d20d21af28012bcb65aea89dd6358
pedro-demeu <pdemeu@segware.com>
2021-12-22 16:41:28 -0300

    Lesson 09

---
+++ src/Aula0009-type-enum/index.ts
@@ -0,0 +1,10 @@
+enum Cores {
+  RED, // 1
+  BLUE, // 2
+  GREEN, // 3
+}
+console.log(Cores);
+// { '0': 'RED', '1': 'BLUE', '2': 'GREEN', RED: 0, BLUE: 1, GREEN: 2 }
+
+console.log(Cores.BLUE); // 1
+console.log(Cores[1]); // BLUE
