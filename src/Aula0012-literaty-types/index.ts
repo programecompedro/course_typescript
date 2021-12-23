c17a65f7b56e09c3b1a9961887d15f656d790748
pedro-demeu <pdemeu@segware.com>
2021-12-23 09:55:05 -0300

    Lesson 12

---
+++ src/Aula0012-literaty-types/index.ts
@@ -0,0 +1,9 @@
+let x = 10; //eslint-disable-line
+x = 0b1010;
+
+const y = 10;
+let a: 100 = 100; //eslint-disable-line
+// a = 120; Não funciona "a" só pode ser 100
+
+// Module mode
+export default 1;
