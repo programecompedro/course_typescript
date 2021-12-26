d43bf1c44411b0fcb82151f879a1823f9989da1e
pedro-demeu <pdemeu@segware.com>
2021-12-26 17:12:35 -0300

    Lesson 21 about POO

---
+++ src/Aula0021-POO/index.ts
@@ -0,0 +1,36 @@
+export class Empresa {
+  public readonly nome: string; // public não necessário
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador.nome, colaborador.sobrenome);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string
+  ) {}
+}
+
+const empresa1 = new Empresa("DemeuSoftware", "11.111.111/0001-11");
+const colaborador1 = new Colaborador("Pedro", "Miguel");
+const colaborador2 = new Colaborador("Pedro", "Neves");
+
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+
+empresa1.mostrarColaboradores();
